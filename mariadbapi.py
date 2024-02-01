e87d307927f5be2166d62cb69fe7b51c42e7d114
Anupam Singha Roy <Anupam.Roy@rishabhsoft.com>
2024-02-01 16:26:33 +0530

    2nd commit

---
+++ mariadbapi.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+import mariadb
+
+app = Flask(__name__)
+
+#MariaDB configurations
+#Replace below valus with your connections
+db_config = {    
+        'host': '', 
+        'port': 3306,
+        'user': 'root',    
+        'password': '', #mariadb setup password 
+        'database': 'flask_maria'# replace this with database name which you have created in  MariaDB
+}
+
+# Endpoint to get data
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    try:
+        conn = mariadb.connect(**db_config)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM employees")
+        data = cursor.fetchall()
+        print(data)
+        conn.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+#Endpoint to insert data
+@app.route('/insert_data', methods=['GET','POST'])
+def insert_data():
+    try:
+        data = request.json
+        employee_id = data['id']
+        first_name = data['fn']
+        last_name = data['ln']
+        job_title = data['jt']
+        salary = data['sl']
+
+        conn = mariadb.connect(**db_config)
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO  employees VALUES (?, ?, ?, ?, ?)", (employee_id,first_name,last_name,job_title,salary))
+        conn.commit()
+
+        conn.close()
+        return jsonify({'message': 'Data inserted successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
